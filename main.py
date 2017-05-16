d5f38e5bfbb6a97db111b437bdc6593e0a6b3ba8
jundialwan <jundi.alwan@gmail.com>
2017-05-16 22:39:14 +0700

    finalize main.py

--- main.py
+++ main.py
@@ -1,45 +1,68 @@
-import numpy as np
+import re
+import csv
 from data import load_data
-from sklearn.feature_extraction.text import TfidfTransformer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from stemming.porter2 import stem
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import AdaBoostClassifier #For Classification
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import LinearSVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import MultinomialNB # .61
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 from sklearn.neural_network import MLPClassifier
-from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 from sklearn.pipeline import Pipeline
 
+csv.field_size_limit(500000)
 
 # load data from given csv file
 train_sentences, train_labels = load_data('fake_train.csv')
 
-# change every sentences in the corpus to TF-IDF vector
-# v = TfidfVectorizer(smooth_idf=False)
-# vectorizer = TfidfVectorizer(stop_words='english', smooth_idf=False)
-# train_tfidf_vector = vectorizer.fit_transform(train_sentences)
+countries = []
 
-# begin train the model
-# model = LogisticRegression().fit(train_tfidf_vector, train_labels) #.77
-# model = KNeighborsClassifier(n_neighbors=3).fit(train_tfidf_vector, train_labels) # .61
-# model = MultinomialNB().fit(train_tfidf_vector, train_labels) # .33
-# model = RandomForestClassifier().fit(train_tfidf_vector, train_labels) # .62
-# model = MLPClassifier().fit(train_tfidf_vector, train_labels) # .70
+with open('countries.csv', 'rU') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        try:
+            countries.append(row['country'])
+        except:
+            continue
+
+cleaned_train_sentences = []
+
+for sentence in train_sentences:
+    free_dot_sentence_tokens = word_tokenize(re.sub('[.,]', '', sentence))
+    free_stopwords_sentence = []
+    stpwrds = set(stopwords.words('english'))
+
+    for word in free_dot_sentence_tokens:
+        if word not in stpwrds:
+            free_stopwords_sentence.append(word)
+
+    cleaned_train_sentences.append(' '.join(free_stopwords_sentence))
+
+# algoritm used
+#   LogisticRegression()
+#   KNeighborsClassifier(n_neighbors=3)
+#   MultinomialNB()
+#   RandomForestClassifier()
+#   MLPClassifier()
+
+algoritm = LogisticRegression()
 
 model = Pipeline([
-    ('vectorizer', TfidfVectorizer(stop_words=None, smooth_idf=False)),
-    ('classifier', MultinomialNB())])
+    ('vectorizing', CountVectorizer(stop_words='english')),
+    ('classifying', algoritm)])
 
-model.fit(train_sentences, train_labels)
+model.fit(cleaned_train_sentences, train_labels)
 
 # load test data from given csv
 test_sentences, test_labels = load_data('fake_test.csv')
 
-# test_tfidf_vector = vectorizer.transform(test_sentences)
-
-# prediction = model.predict(test_tfidf_vector)
 prediction = model.predict(test_sentences)
 
 accuracy = accuracy_score(test_labels, prediction)
